d1a08031f3286ed52ef5285b2d3a129e00e7c423
Devin Lehmacher <lehmacdj@gmail.com>
2017-10-16 13:02:42 -0400

    add some capability checking

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+cargo >/dev/null 2>&1 || (echo "cargo / rust is not installed!" && exit 1)
+mdbook --help >/dev/null 2>&1 || cargo install mdbook
 mdbook build
 git checkout gh-pages
 mv book .book
