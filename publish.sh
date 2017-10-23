5736dabc8ca54204f9de94118228ac501287f677
Devin Lehmacher <lehmacdj@gmail.com>
2017-10-23 17:51:21 -0400

    make publish script not delete submodule

--- publish.sh
+++ publish.sh
@@ -6,10 +6,13 @@ mdbook --help >/dev/null 2>&1 || cargo install mdbook
 mdbook build
 git checkout gh-pages
 mv book .book
+mv haskell-through-diagrams .haskell-through-diagrams
 rm -rf -- *
 mv .book/* .
+mv .haskell-through-diagrams haskell-through-diagrams
 rmdir .book
 git add -- * || :
+git reset haskell-through-diagrams || :
 git commit -m "update published book pages" || :
 git push || :
 git checkout master
