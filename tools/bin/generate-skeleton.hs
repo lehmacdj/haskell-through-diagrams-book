7e8c43e037a1c965961978a164db896d3d6bc46d
Devin Lehmacher <lehmacdj@gmail.com>
2017-11-14 17:23:38 -0500

    implement support for sub-chapters

--- tools/bin/generate-skeleton.hs
+++ tools/bin/generate-skeleton.hs
@@ -10,11 +10,9 @@ paddedNumber n
   | otherwise = show n
 
 linkText s = intercalate "-" $ filter isAlphaNum . map toLower <$> words s
-qualifiedLink i s = paddedNumber i ++ "-" ++ linkText s ++ ".md"
-unqualifiedLink s = linkText s ++ ".md"
+mkLink is s = ((\i -> paddedNumber i ++ "-") =<< is) ++ linkText s ++ ".md"
 reference i = "[" ++ show i ++ "]"
-makeBullet i s = "- [" ++ s ++ "]" ++ "(" ++ qualifiedLink i s ++ ")"
-makePrologue s = "[" ++ s ++ "]" ++ "(" ++ unqualifiedLink s ++ ")"
+makePrologue s = "[" ++ s ++ "]" ++ "(" ++ link (PQ [] s) ++ ")"
 
 header = ["# Summary", ""]
 title s = "# " ++ s
@@ -22,31 +20,42 @@ skeletonPrefix = "./src/"
 
 generatePrologue :: String -> IO ()
 generatePrologue s = do
-    let link = unqualifiedLink s
+    let link = mkLink [] s
         file = skeletonPrefix ++ link
     exists <- doesFileExist file
     unless exists $ writeFile file $ title s
 
-generateChapter :: Int -> String -> IO ()
-generateChapter i s = do
-    let link = qualifiedLink i s
-        file = skeletonPrefix ++ link
+link :: PageQual -> String
+link (PQ is s) = mkLink is s
+
+data PageQual = PQ [Int] String
+
+generatePage pq@(PQ is s) = do
+    let file = skeletonPrefix ++ link pq
     exists <- doesFileExist file
     unless exists $ writeFile file $ title s
+
+chapterize :: [String] -> [Int -> [PageQual]]
+chapterize [] = []
+chapterize (x:xs) = (\i -> PQ [i] x:makeSub i xs') : chapterize ys' where
+    (xs', ys') = span ("    " `isPrefixOf`) xs
+    makeSub i = zipWith (\x s -> PQ [i, x] (drop 4 s)) [1..]
+
+makeBullet' :: PageQual -> String
+makeBullet' pq@(PQ is s) = prefix ++ "- [" ++ s ++ "]" ++ "(" ++ link pq ++ ")"
+    where prefix = replicate (4 * (length is - 1)) ' '
 
 main :: IO ()
 main = do
     index <- readFile "chapter-index"
-    let entries = lines index
-        pEntries' = takeWhile (not . null) entries
-        cEntries' = drop 1 $ dropWhile (/="") entries
-        pEntries = filter (not . ("#" `isPrefixOf`)) pEntries'
-        cEntries = filter (not . ("#" `isPrefixOf`)) cEntries'
+    let entries = filter (not . ("#" `isPrefixOf`)) $ lines index
+        pEntries = takeWhile (not . null) entries
+        cEntries = drop 1 $ dropWhile (/="") entries
+        cEntries' = concat $ zipWith ($) (chapterize cEntries) [1..]
         indexes = [1..]
         prologue = map makePrologue pEntries
-        chapters = zipWith makeBullet indexes cEntries
-        skeletons = map generatePrologue pEntries
-            ++ zipWith generateChapter indexes cEntries
+        chapters = map makeBullet' cEntries'
+        skeletons = map generatePrologue pEntries ++ map generatePage cEntries'
     writeFile "src/SUMMARY.md" $ unlines $
         header ++ prologue ++ [""] ++ chapters
     sequence_ skeletons
